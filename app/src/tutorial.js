7bf3d1adc3e78d4552b3392c7774bebbe64001e7
kyokutyo <haruta@kyokutyo.com>
2014-10-05 10:23:37 +0900

    CommentList, CommentForm を作る

--- app/src/tutorial.js
+++ app/src/tutorial.js
@@ -1,13 +1,36 @@
 /** @jsx React.DOM */
-var CommentBox = React.createClass({
+var CommentList = React.createClass({
     render: function() {
         return (
-            <div className="commentBox">
-                Hello, world! I am a CommentBox.<br />
+            <div className="commentList">
+                Hello, world! I am a CommentList.
             </div>
         );
     }
 });
+
+var CommentForm = React.createClass({
+    render: function() {
+        return (
+            <div className="commentForm">
+                Hello, world! I am a CommentForm.
+            </div>
+        );
+    }
+});
+
+var CommentBox = React.createClass({
+    render: function() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
+            </div>
+        );
+    }
+});
+
 React.renderComponent(
     <CommentBox />,
     document.getElementById('content')
