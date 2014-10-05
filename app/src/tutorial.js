e298a3b18d87ca5c6115c8f7ce32b96b67558af3
kyokutyo <haruta@kyokutyo.com>
2014-10-05 10:59:53 +0900

    データを JSON ファイルに外出しする

--- app/src/tutorial.js
+++ app/src/tutorial.js
@@ -1,9 +1,4 @@
 /** @jsx React.DOM */
-var data = [
-    {author: "Pete Hunt", text: "This is comment"},
-    {author: "Jordan Walke", text: "This is *another* comment"}
-];
-
 var converter = new Showdown.converter();
 var Comment = React.createClass({
     render: function() {
@@ -47,11 +42,26 @@ var CommentForm = React.createClass({
 });
 
 var CommentBox = React.createClass({
+    getInitialState: function() {
+        return {data: []}
+    },
+    componentDidMount: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            success: function(data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
     render: function() {
         return (
             <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList data={this.props.data} />
+                <CommentList data={this.state.data} />
                 <CommentForm />
             </div>
         );
@@ -59,6 +69,6 @@ var CommentBox = React.createClass({
 });
 
 React.renderComponent(
-    <CommentBox data={data} />,
+    <CommentBox url="comments.json" />,
     document.getElementById('content')
 );
