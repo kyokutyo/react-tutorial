d6db1e335c3cad0dab92a0a4fa5a5e6d12b5da52
kyokutyo <haruta@kyokutyo.com>
2014-10-05 12:17:05 +0900

    (POST を受け付けるていで)コメントを投稿する

--- app/src/tutorial.js
+++ app/src/tutorial.js
@@ -69,7 +69,18 @@ var CommentBox = React.createClass({
         });
     },
     handleCommentSubmit: function(comment) {
-        console.log(comment.author, comment.text);
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            type: 'POST',
+            data: comment,
+            success: function(data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
     },
     getInitialState: function() {
         return {data: []}
