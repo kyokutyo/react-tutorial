9d55826aab617d3f23ab7c647981a837745406e4
kyokutyo <haruta@kyokutyo.com>
2014-10-05 10:17:53 +0900

    gulp で JSX コンパイルをする

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
+var react = require('gulp-react');
 
-//Webサーバー
 gulp.task('webserver', function() {
-    gulp.src('app')
-        .pipe(webserver({
-            livereload: true,
-        }));
+    return gulp.src('app')
+    .pipe(webserver({
+        livereload: true,
+    }));
 });
 
-gulp.task('default', ['webserver']);
+gulp.task('watch', function () {
+    return gulp.watch('app/src/*.js', ['build-react']);
+});
+
+gulp.task('build-react', function () {
+    return gulp.src('app/src/*.js')
+    .pipe(react())
+    .pipe(gulp.dest('app/build'));
+});
+
+gulp.task('default', ['webserver', 'watch']);
